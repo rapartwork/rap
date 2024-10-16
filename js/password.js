231ad1ef8497a45d005534b3e58cd05c7d13afd1
rapartwork <isabel.donnelly@holmesglen.edu.au>
2024-10-17 10:31:45 +1100

    Update password.js

--- js/password.js
+++ js/password.js
@@ -2,7 +2,7 @@
 		document.getElementById('passwordButton').addEventListener('click', function() {
 			var password = document.getElementById('passwordInput').value;
 			var errorMessage = document.getElementById('errorMessage');
-			if (password === 'yamaha97') {
+			if (password === 'rapart') {
 				document.getElementById('passwordOverlay').style.display = 'none';
 			} else {
 				errorMessage.textContent = 'Incorrect password. Try again.';
